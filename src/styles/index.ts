4ff082708fd2c7d6435e9369149f0c02337ac204
Patrick Nekel <nekelpatrick@gmail.com>
2022-12-30 23:22:04 -0300

    update color tokens

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -12,8 +12,11 @@ export const {
 } = createStitches({
   theme: {
     colors: {
+      gray700: "#F4F4F4",
+      gray600: "#F5F5F5",
+      gray550: "#EAEDF0",
       gray500: "#DFE1E4",
-
+      gray400: "#6C6F75",
       brandPrimary500: "#4734FE",
       brandSucess500: "#78C552",
 
