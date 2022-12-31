efb35443cd1936cdfa4cfc809ccd47f4c8030ad6
Patrick Nekel <nekelpatrick@gmail.com>
2022-12-31 15:37:53 -0300

    add rule so next can ignore the random user img domain

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+  // reactStrictMode: true,
+  images:{
+    domains: ['randomuser.me']
+  }
+
 }
 
-module.exports = nextConfig
