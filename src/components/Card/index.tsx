606088ca383a4a366a9240d41f2f81308ed3a896
Patrick Nekel <nekelpatrick@gmail.com>
2022-12-31 11:49:24 -0300

    add card basic structure

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,4 @@
+import { Card } from "./styles";
+export const CardComponent = () => {
+  return <Card />;
+};
---
+++ src/components/Card/styles.ts
@@ -0,0 +1,9 @@
+import { styled } from "../../styles/index";
+
+export const Card = styled("div", {
+  width: "244px",
+  minHeight: "130px",
+  maxHeight: "238px",
+  backgroundColor: "red",
+  margin: "20px 0 0 20px",
+});
