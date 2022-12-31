236bc89b66be916ef61d9e0f434f58879fecaa90
Patrick Nekel <nekelpatrick@gmail.com>
2022-12-30 23:21:36 -0300

    updating home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,12 @@
+import { ToolbarComponent } from "../components/Toolbar";
+import { Card, HomeContainer } from "../styles/pages/home";
+
 export default function Home() {
-  return <h1>Hello</h1>;
+  return (
+    <HomeContainer>
+      <ToolbarComponent />
+      <h1>Hey</h1>
+      <h1>Hey</h1> <h1>Hey</h1>
+    </HomeContainer>
+  );
 }
