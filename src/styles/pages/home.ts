79788eb151ddbf486bc86b3fe4fb1d94f79a4e63
Patrick Nekel <nekelpatrick@gmail.com>
2022-12-30 23:22:33 -0300

    add home page styling

---
+++ src/styles/pages/home.ts
@@ -0,0 +1,16 @@
+import { styled } from "..";
+
+export const HomeContainer = styled("main", {
+  position: "absolute",
+  top: "54px",
+  left: "56px",
+  right: "0",
+  bottom: "0",
+  marginTop: "24px",
+  marginLeft: "24px",
+  // width: "100%",
+});
+
+export const ProductContainer = styled("div", {});
+
+export const Card = styled("a", {});
