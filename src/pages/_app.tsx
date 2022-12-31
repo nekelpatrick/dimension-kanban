019bf8ee66c5bfeacafd53f0c8887b0c613e312e
Patrick Nekel <nekelpatrick@gmail.com>
2022-12-30 23:21:17 -0300

    add components to the UI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,21 @@
 import type { AppProps } from "next/app";
+import Image from "next/image";
+import { NavBar } from "../components/NavBar";
+import { Products } from "../components/Products";
+
+import { Container, globalStyles } from "../styles/Global";
+import { Header } from "../styles/pages/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  globalStyles();
+
+  return (
+    <>
+      <Container>
+        <NavBar />
+        <Products />
+        <Component {...pageProps} />
+      </Container>
+    </>
+  );
 }
