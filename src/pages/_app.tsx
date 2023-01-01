990611370e69af2b3f4236d2d2b7353b57ceb925
Patrick Nekel <nekelpatrick@gmail.com>
2023-01-01 16:06:37 -0300

    add custom title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,17 @@ import { NavBar } from "../components/Navbar";
 import { Products } from "../components/Products";
 
 import { Container, globalStyles } from "../styles/Global";
-import { Header } from "../styles/pages/app";
+
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   globalStyles();
 
   return (
     <>
+      <Head>
+        <title>Dimension | Kanban Board</title>
+      </Head>
       <Container>
         <NavBar />
         <Products />
