998aed2b9589535a598d55984d734ccb674c3b52
Patrick Nekel <nekelpatrick@gmail.com>
2023-01-02 00:21:33 -0300

    fix type errors in the kanban function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -41,17 +41,6 @@ interface Item {
   // add properties for the item object here
 }
 
-interface Re {
-  source: {
-    droppableId: string;
-    index: number;
-  };
-  destination?: {
-    droppableId: string;
-    index: number;
-  };
-}
-
 export function Home() {
   const [selectedBoard, setSelectedBoard] = useState(0);
   const [boardData, setBoardData] = useState(BoardData);
@@ -80,7 +69,7 @@ export function Home() {
   //   setBoardData(newBoardData);
   // };
 
-  const onDragEnd = (re: Re) => {
+  const onDragEnd = (re: any) => {
     // check if there is no destination, then return early
     if (!re.destination) return;
 
@@ -142,70 +131,6 @@ export function Home() {
       }
     }
   };
-
-  // return (
-  //   <HomeContainer>
-  //     <ToolbarComponent />
-  //     {ready && (
-  //       <DragDropContext onDragEnd={onDragEnd}>
-  //         <ColumnArea className="grid grid-cols-4 gap-5 my-5">
-  //           {boardData.map((board, bIndex) => (
-  //             <Droppable key={board.name} droppableId={bIndex.toString()}>
-  //               {(provided, snapshot) => (
-  //                 <BoardColumn
-  //                   {...provided.droppableProps}
-  //                   ref={provided.innerRef}
-  //                   className={`bg-gradient-to-r from-pink-700 to-red-200 absolute inset-x-0 top-0 ${
-  //                     snapshot.isDraggingOver && "isDraggingOver"
-  //                   }`}
-  //                 >
-  //                   <h4 className="p-3 flex justify-between items-center mb-2">
-  //                     <span className="text-2xl text-gray-600">
-  //                       {board.name}
-  //                     </span>
-  //                   </h4>
-  //                   <BoardCardsArea>
-  //                     {board.items.length > 0 &&
-  //                       board.items.map((item, iIndex) => (
-  //                         <CardComponent
-  //                           key={item.id}
-  //                           data={item}
-  //                           index={iIndex}
-  //                           className="m-3"
-  //                         />
-  //                       ))}
-  //                     {provided.placeholder}
-  //                   </BoardCardsArea>
-  //                   {showForm && selectedBoard === bIndex ? (
-  //                     <AddNewCard className="p-3">
-  //                       <textarea
-  //                         className="border-gray-300 rounded focus:ring-purple-400 w-full"
-  //                         rows={3}
-  //                         placeholder="Task info"
-  //                         data-id={bIndex}
-  //                         onKeyDown={onTextAreaKeyPress}
-  //                       />
-  //                     </AddNewCard>
-  //                   ) : (
-  //                     <AddTaskButton
-  //                       className="flex justify-center items-center my-3 space-x-2 text-lg"
-  //                       onClick={() => {
-  //                         setSelectedBoard(bIndex);
-  //                         setShowForm(true);
-  //                       }}
-  //                     >
-  //                       <span>Add task</span>
-  //                     </AddTaskButton>
-  //                   )}
-  //                 </BoardColumn>
-  //               )}
-  //             </Droppable>
-  //           ))}
-  //         </ColumnArea>
-  //       </DragDropContext>
-  //     )}
-  //   </HomeContainer>
-  // );
 
   return (
     <HomeContainer>
